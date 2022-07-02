acb98a91272d55bc9696023d8781a84cb6190808
aa <aaaa>
2022-07-02 13:01:56 +0900

    memo

--- Myweb/src/main/java/com/example/Myweb/MyController.java
+++ Myweb/src/main/java/com/example/Myweb/MyController.java
@@ -13,18 +13,19 @@ public class MyController {
 		return "index";  //index.html 파일을 열어라
 	}
 
-
 	//오른쪽에 있는 값을 오ㅓ왼쪽으,ㅣ 이름으로 날리는 것 
 	@GetMapping("/oper")  //localhost:8084/oper
-	public String oper(Model model, @RequestParam("num1") String num) {
-		String msg = "숫자를 입력하세요";
-		int age = Integer.parseInt(num);
-		if(age >= 20) {
-			msg = "성인 입니다.";
-		}else if(age >= 14) {
-			msg = "미성년자 입니다.";
+	public String oper(Model model, @RequestParam("id") String id,
+			 @RequestParam("pw") String pw) {
+		String msg = "";
+		if (!(id.equals("hello"))) {
+			msg =  "일치하는 아이디가 없습니다.";
+		}else if(!(pw.equals("world"))) {
+			msg = "비밀번호가 일치하지 않습니다.";
 		}else {
-			msg = "어린이 입니다.";
+			msg = "로그인 성공!";
+			model.addAttribute("result",msg);  //result라는 키워드로 화면에 보내기 
+			return "oper2";
 		}
 		model.addAttribute("result",msg);  //result라는 키워드로 화면에 보내기 
 		return "oper";   //oper.html 열어라
